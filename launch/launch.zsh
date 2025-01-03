1a639b6bfe065a083702c88d9b904539be017749
Wiktor Kowalczyk <wiktormkowalczyk@gmail.com>
2025-01-03 21:10:56 +0100

    ekf skeleton and launch file

--- launch/launch.zsh
+++ launch/launch.zsh
@@ -1,7 +1,3 @@
-conda activate ros_env
-colcon build --symlink-install
-source install/setup.zsh
-
 ros2 run localisation_planning icp &
 ros2 run localisation_planning laser_scan_handler &
 ros2 run localisation_planning laser_scan_debug_publisher &
