3d37ac50ce6d1215b78a614519e8b3425ff97aae
Wiktor Kowalczyk <wiktormkowalczyk@gmail.com>
2025-01-04 19:53:03 +0100

    ekf ported

--- launch.zsh
+++ launch.zsh
@@ -1,8 +1,9 @@
 source install/setup.zsh &
 
-ros2 run odometryicp &
-ros2 run odometrylaser_scan_handler &
-ros2 run odometrylaser_scan_debug_publisher &
-ros2 run odometryekf_slam &
+ros2 run odometry icp &
+ros2 run odometry laser_scan_handler &
+ros2 run odometry laser_scan_debug_publisher &
+ros2 run odometry ekf_slam &
+ros2 run slam_ekf slam_ekf &
 
 wait
