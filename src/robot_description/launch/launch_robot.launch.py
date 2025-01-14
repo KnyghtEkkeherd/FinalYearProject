70076eff9a6ff46ac4042ae23cdecdcf736ddd29
Wiktor Kowalczyk <wiktormkowalczyk@gmail.com>
2025-01-14 16:26:43 +0100

    changed urdf model

---
+++ src/robot_description/launch/launch_robot.launch.py
@@ -0,0 +1,56 @@
+import launch
+from launch.launch_description_sources.python_launch_description_source import PythonLaunchDescriptionSource
+from launch.substitutions import Command, LaunchConfiguration
+import launch_ros
+import os
+
+def generate_launch_description():
+    pkg_share = launch_ros.substitutions.FindPackageShare(package='robot_description').find('robot_description')
+    default_model_path = os.path.join(pkg_share, 'src/description/robot_description.urdf')
+    default_rviz_config_path = os.path.join(pkg_share, 'rviz/urdf_config.rviz')
+    world_path=os.path.join(pkg_share, 'world/my_world.sdf')
+
+    robot_state_publisher_node = launch_ros.actions.Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}, {'use_sim_time': LaunchConfiguration('use_sim_time')}]
+    )
+    rviz_node = launch_ros.actions.Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', LaunchConfiguration('rvizconfig')],
+    )
+    spawn_entity = launch_ros.actions.Node(
+    	package='gazebo_ros',
+    	executable='spawn_entity.py',
+        arguments=['-entity', 'robot', '-topic', 'robot_description'],
+        output='screen'
+    )
+    robot_localization_node = launch_ros.actions.Node(
+         package='robot_localization',
+         executable='ekf_node',
+         name='ekf_filter_node',
+         output='screen',
+         parameters=[os.path.join(pkg_share, 'config/ekf.yaml'), {'use_sim_time': LaunchConfiguration('use_sim_time')}]
+    )
+
+    ekf_slam = IncludeLaunchDescription(PythonLaunchDescriptionSource(
+        [os.path.join(get_package_share_directory('slam_toolbox'), 'launch', 'online_async.launch.py')]),
+        launch_arguments={'use_sim_time': 'true'}.items())
+
+    return launch.LaunchDescription([
+        launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
+                                            description='Absolute path to robot urdf file'),
+        launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
+                                            description='Absolute path to rviz config file'),
+        launch.actions.DeclareLaunchArgument(name='use_sim_time', default_value='True',
+                                            description='Flag to enable use_sim_time'),
+        launch.actions.ExecuteProcess(cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so', world_path], output='screen'),
+        robot_state_publisher_node,
+        spawn_entity,
+        robot_localization_node,
+        rviz_node,
+        ekf_slam
+    ])
