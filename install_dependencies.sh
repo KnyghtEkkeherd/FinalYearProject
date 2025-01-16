8300a88e0a1a8e1dd9e01331d85851e5d1d1d422
Wiktor Kowalczyk <wiktormkowalczyk@gmail.com>
2025-01-16 12:49:38 +0100

    dependencies installation script

---
+++ install_dependencies.sh
@@ -0,0 +1,3 @@
+sudo apt update
+sudo apt upgrade -y
+sudo apt install -y ros-jazzy-nav2-minimal-tb* ros-jazzy-slam-toolbox ros-jazzy-turtlebot3* ros-jazzy-ros2-control ros2-jazzy-controllers ros-jazzy-gazebo-ros2-control
