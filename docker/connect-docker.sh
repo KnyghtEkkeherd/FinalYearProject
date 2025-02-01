7c9724ada26cc6b70a0b46a58b8e34444ffcea41
Ujaan Das <ujaandas03@gmail.com>
2025-02-01 12:50:34 +0530

    add rpicam

--- docker/connect-docker.sh
+++ docker/connect-docker.sh
@@ -8,12 +8,27 @@ echo "Connecting to the ROS2 Ubuntu container in the project directory: $PROJECT
 # Enable GUI
 xhost +
 
-# Check if the container is running
-if [ "$(docker-compose -f $PROJECT_DIR/docker-compose.yml ps | grep 'ros2ubuntu.*Up')" ]; then
-    echo "The container is already running. Connecting to the container..."
-    docker-compose -f $PROJECT_DIR/docker-compose.yml exec ros2ubuntu bash
-else
-    echo "The container is not running. Starting the container..."
-    docker-compose -f $PROJECT_DIR/docker-compose.yml up -d
-    docker-compose -f $PROJECT_DIR/docker-compose.yml exec ros2ubuntu bash
+# Build the Docker image
+SCRIPT_PATH=$(dirname $(realpath "$0"))
+PARENT_PATH=$(dirname "$SCRIPT_PATH")
+
+echo "Building the Docker image..."
+sudo docker image build --no-cache=false -f $SCRIPT_PATH/Dockerfile -t ros2ubuntu:latest $PARENT_PATH
+
+# Stop and remove the old container if it exists
+if [ "$(docker ps -q -f name=ros2ubuntu)" ]; then
+    echo "Stopping and removing the old container..."
+    docker stop ros2ubuntu
+    docker rm ros2ubuntu
 fi
+
+# Remove old images
+echo "Removing old images..."
+docker image prune -f
+
+# Start the container
+echo "Starting the container..."
+docker-compose -f $PROJECT_DIR/docker-compose.yml up -d
+
+# Connect to the container
+docker-compose -f $PROJECT_DIR/docker-compose.yml exec ros2ubuntu bash
