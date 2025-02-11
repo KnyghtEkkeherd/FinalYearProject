e5222d9cc3ee3eb8da5930e23de0d5f95f7cdc47
Wiktor Kowalczyk <wiktormkowalczyk@gmail.com>
2025-02-11 11:30:08 +0800

    added intermediate node

--- src/twistHandler/twistHandler/twistHandler.py
+++ src/twistHandler/twistHandler/twistHandler.py
@@ -1,20 +1,19 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import String
 from geometry_msgs.msg import Twist, TwistStamped
 
 class twistHandler (Node):
 
     def __init__(self):
-        super().__init__('minimal_pubsub_node')
-        self.publisher_ = self.create_publisher(TwistStamped, '/diff_cont/cmd_vel', 10)
+        super().__init__('twistHandler')
+        self.publisher = self.create_publisher(TwistStamped, '/diff_cont/cmd_vel', 10)
         self.subscription = self.create_subscription(Twist, '/cmd_vel_smoothed', self.listener_callback, 10)
 
     def listener_callback(self, msg):
         twist_stamped_msg = TwistStamped()
         twist_stamped_msg.header.stamp = self.get_clock().now().to_msg()
         twist_stamped_msg.twist = msg
-        self.publisher_.publish(twist_stamped_msg)
+        self.publisher.publish(twist_stamped_msg)
 
 def main(args=None):
     rclpy.init(args=args)
