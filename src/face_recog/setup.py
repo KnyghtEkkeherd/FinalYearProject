a27f70914caa5a5c614fa015ad9198d87e1ba1af
armaanisfab <armaanisfab@gmail.com>
2025-03-07 17:55:15 +0800

    Update setup.py

--- src/face_recog/setup.py
+++ src/face_recog/setup.py
@@ -10,7 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name, ['embeddings.json']),
+        ('share/' + package_name, ['/face_recog/embeddings.json']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
