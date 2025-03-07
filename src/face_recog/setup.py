944f2179d3bbd6d1d416b37d6a6351d08b47a13b
armaanisfab <armaanisfab@gmail.com>
2025-03-07 17:05:44 +0800

    embeddings.json

--- src/face_recog/setup.py
+++ src/face_recog/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'face_recog = face_recog.face_recognition_node_yolo:main',
         ],
     },
 )
