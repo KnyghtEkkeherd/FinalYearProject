8a09468b73cf8abeec139c52523a5505d62fab6e
Raspberry Pi <wmkowalczyk@connect.ust.hk>
2025-03-11 12:53:10 +0800

    servos work

--- src/gpio_handler/gpio_handler/gpio_node.py
+++ src/gpio_handler/gpio_handler/gpio_node.py
@@ -4,13 +4,14 @@ from rclpy.node import Node
 from gpio_interface.msg import ServoCmd
 from gpio_interface.srv import ServoInit, ServoSet
 import lgpio
+    
+handle = lgpio.gpiochip_open(4)
 
 class ServoHandler(Node):
     def __init__(self):
         super().__init__('gpio_node')
         # {servo_id: gpio pin}
         self.servo_gpios = {}
-        self.handle = lgpio.gpiochip_open(4)
         self.servo_pwm_min = 500
         self.servo_pwm_max = 2500
         self.servo_count = 0
@@ -18,14 +19,7 @@ class ServoHandler(Node):
 
         self.servo_cmd = self.create_subscription(ServoCmd, '/servo/cmd', self.servo_set, 10)
         self.servo_initialize_srv = self.create_service(ServoInit, 'servo_init', self.servo_init)
-        self.servo_cmd_srv = self.create_service(ServoSet, 'servo_cmd', self.servo_set_srv)
-
-    def __del__(self):
-        try:
-            lgpio.gpiochip_close(self.handle)
-            self.get_logger().info("GPIO chip closed successfully")
-        except Exception as e:
-            self.get_logger().error(f"Error closing GPIO chip: {e}")
+        self.servo_cmd_srv = self.create_service(ServoSet, 'servo_set', self.servo_set_srv)
 
     def servo_init(self, init_request, response):
         servo_gpio = init_request.servo_gpio
@@ -33,15 +27,19 @@ class ServoHandler(Node):
         if (servo_gpio in self.servo_gpios.values()):
             self.get_logger().error(f"Servo {servo_gpio} already initialized")
             response.servo_id = -1
+            return response
         try:
-            lgpio.gpio_claim_output(self.handle, servo_gpio)
-            lgpio.tx_servo(self.handle, servo_gpio, 0)
+            lgpio.gpio_claim_output(handle, servo_gpio)
+            lgpio.tx_servo(handle=handle, gpio=servo_gpio, pulse_width=1500)
             self.servo_gpios[self.servo_count] = servo_gpio
             self.get_logger().info(f"Servo {self.servo_count} initialized")
             response.servo_id = self.servo_count
             self.servo_count += 1
+            return response
         except Exception as e:
             self.get_logger().error(f"Error initializing servo {self.servo_count} on GPIO:{servo_gpio}: {e}")
+            response.servo_id = -1
+            return response
 
     def servo_set(self, cmd_msg):
         servo_id = cmd_msg.servo_id
@@ -50,15 +48,15 @@ class ServoHandler(Node):
             self.get_logger().error(f"Servo {servo_id} not initialized")
             return False
         try:
-            lgpio.tx_servo(self.handle, self.servo_gpios[servo_id], self.servo_pwm_min + self.servo_pwm_step*angle)
+            lgpio.tx_servo(handle=handle, gpio=self.servo_gpios[servo_id], pulse_width=self.get_pulse(angle=angle))
             return True
         except Exception as e:
             self.get_logger().error(f"Error setting servo {servo_id}: {e}")
+            return False
 
     def servo_set_srv(self, cmd_msg_request, response):
         response.success = self.servo_set(cmd_msg_request)
         return response
-
 
     def cmd_callback(self, cmd_msg):
         servo_id = cmd_msg.servo_id
@@ -67,9 +65,15 @@ class ServoHandler(Node):
             self.get_logger().error(f"Servo {servo_id} not initialized")
             return -1
         try:
-            lgpio.tx_servo(self.handle, self.servo_gpios[servo_id], self.servo_pwm_min + self.servo_pwm_step*angle)
+            lgpio.tx_servo(handle=handle, gpio=self.servo_gpios[servo_id], pulse_width=self.get_pulse(angle=angle))
         except Exception as e:
             self.get_logger().error(f"Error setting servo {servo_id} on GPIO:{self.servo_gpios[servo_id]}: {e}")
+            return -1
+        
+    # Helpers:
+    def get_pulse(self, angle):
+        pulse = angle
+        return pulse
 
 def main(args=None):
     rclpy.init(args=args)
@@ -78,6 +82,7 @@ def main(args=None):
 
     servo_handler.destroy_node()
     rclpy.shutdown()
+    lgpio.gpiochip_close(handle)
 
 if __name__ == '__main__':
     main()
