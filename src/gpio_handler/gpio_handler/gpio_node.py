f18a6e858e07989c8778db25c118107258c5f7ce
armaanisfab <armaanisfab@gmail.com>
2025-03-24 18:07:27 +0800

    Update gpio_node.py

--- src/gpio_handler/gpio_handler/gpio_node.py
+++ src/gpio_handler/gpio_handler/gpio_node.py
@@ -44,7 +44,7 @@ class GpioHandler(Node):
             lgpio.tx_servo(
                 handle=handle,
                 gpio=servo_gpio,
-                pulse_width=int((pulse_max-pulse_min)/4+pulse_min),
+                pulse_width=1500,#int((pulse_max-pulse_min)/4+pulse_min),
                 pulse_cycles=10)
 
             self.servos[len(self.servos)] = (servo_gpio, pulse_min, pulse_max, servo_range)
