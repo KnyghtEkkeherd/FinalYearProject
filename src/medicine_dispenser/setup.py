f889166da423c4532be6e5a572d1bc260a6e7365
armaanisfab <armaanisfab@gmail.com>
2025-04-22 14:49:56 +0800

    Update setup.py

--- src/medicine_dispenser/setup.py
+++ src/medicine_dispenser/setup.py
@@ -5,7 +5,7 @@ package_name = 'medicine_dispenser'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=['medicine_dispenser'],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
