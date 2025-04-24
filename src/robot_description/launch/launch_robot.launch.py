90c94a09c46e4c73b5d0e2224624f124e22b8dfc
oojer patoojer <ujaandas03@gmail.com>
2025-04-24 03:24:37 -0400

    ooga booga fixes

--- src/robot_description/launch/launch_robot.launch.py
+++ src/robot_description/launch/launch_robot.launch.py
@@ -1,48 +1,43 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
-
-
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.actions import IncludeLaunchDescription, TimerAction, RegisterEventHandler
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import Command
-from launch.actions import RegisterEventHandler
 from launch.event_handlers import OnProcessStart
-
 from launch_ros.actions import Node
 
 
-
 def generate_launch_description():
-
-
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
-    package_name='robot_description' #<--- CHANGE ME
+    package_name = 'robot_description'
 
     rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
+        PythonLaunchDescriptionSource([
+            os.path.join(get_package_share_directory(package_name), 'launch', 'rsp.launch.py')
+        ]),
+        launch_arguments={
+            'use_sim_time': 'false',  
+            'use_ros2_control': 'true'  
+        }.items()
     )
 
-    # add twist unstampted to twist stamped node here
+    # ! apparently, im a ghost?
+    # controller_params_file = os.path.join(get_package_share_directory(package_name), 'config', 'my_controllers.yaml') # --> NO WORK
+    controller_params_file = "/tmp/my_controllers.yaml" # --> WORKS BTW LMAO
+    # controller_params_file = "/home/gyattbot/FinalYearProject/src/robot_description/config/my_controllers.yaml" # --> NO WORK!?!?!?!?
+
+    print(f"ctlr param file: {controller_params_file}")
+
     unstampedTwistToStampedTwist = Node(
         package="twistHandler",
         executable="twistHandler",
     )
 
-    robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
-
-    controller_params_file = os.path.join(get_package_share_directory(package_name),'config','my_controllers.yaml')
-
+    # !!! I AM SHITTING MYSELF !!!
     controller_manager = Node(
         package="controller_manager",
         executable="ros2_control_node",
-        parameters=[{'robot_description': robot_description},
-                    controller_params_file]
+        parameters=[controller_params_file],  
     )
 
     delayed_controller_manager = TimerAction(period=3.0, actions=[controller_manager])
@@ -73,7 +68,6 @@ def generate_launch_description():
         )
     )
 
-    # Launch them all!
     return LaunchDescription([
         rsp,
         unstampedTwistToStampedTwist,
